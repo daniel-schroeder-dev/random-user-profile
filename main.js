7c1523ede9ee968edb30984e78eae27e47aa0ccb
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-30 19:58:38 -0500

    Implement api call with fetch to get random user data.

---
+++ main.js
@@ -0,0 +1,24 @@
+const button = document.querySelector('button');
+const img = document.querySelector('.user-profile-card__img');
+const nameSpan = document.querySelector('.user-profile-card__description__name');
+const ageSpan = document.querySelector('.user-profile-card__description__age');
+const locationSpan = document.querySelector('.user-profile-card__description__location');
+
+button.addEventListener('click', fetchAndUpdateUserInformation);
+
+function fetchAndUpdateUserInformation() {
+  
+  const url = 'https://randomuser.me/api/';
+  
+  fetch(url)
+    .then(res => res.json())
+    .then(({ results }) => {
+      const user = results[0];
+      img.src = user.picture.large;
+      nameSpan.textContent = user.name.first;
+      ageSpan.textContent = user.dob.age;
+      locationSpan.textContent = user.location.country;
+    })
+    .catch(console.error)
+
+}
